87cedb5ffb55b49e94ba89614915d8f0d693d026
mdleiton <mau_leiton96@hotmail.com>
2017-08-06 17:26:26 -0500

    agregando funcion para manipular hilo

--- buscar.c
+++ buscar.c
@@ -10,10 +10,12 @@
 
 #define MAX 1000000
 #define MAXPALABRASBUSCAR 100
-pthread_mutex_t mutex; 
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER; //inicializando mutex
 int num_palabras[MAXPALABRASBUSCAR];
 char *palabras[MAXPALABRASBUSCAR];
 int numero_palabras=0;
+
+
 //retorna el numero de lineas del archivo y guarda en tam_lineas el numero de caracteres de cada linea
 int numero_lineas (char *ruta, int *tam_lineas){
 	if (ruta != NULL){
@@ -45,6 +47,38 @@ typedef struct mi_estructuraTDA{
 	FILE * fp;
 	int *tam_lineas;
 } estructura;
+
+//funcion a enviar a cada hilo para que procese una porcion del arreglo
+void * contarpalabras(void *arg){
+	estructura *argumentos = (estructura *)arg;		//convertimos al tipo de dato correcto
+	int ini=argumentos->iniciocaracter;
+    for(int i = argumentos->inilinea; i <= argumentos->finlinea; i++){ 
+		int cant = argumentos->tam_lineas[i]+1;
+		char *buffer = (char *)malloc(cant*sizeof(char));
+		char * token;
+		//region critica
+		pthread_mutex_lock(&mutex);
+		fseek(argumentos->fp,ini,SEEK_SET);
+		fgets(buffer, cant,argumentos->fp);
+		pthread_mutex_unlock(&mutex);
+		//fin region critica
+		while( (token = strtok_r(buffer," \n\e\t\\\?\f\'\"\v\b\r!^~><·$%&/,()=º¡¢£¤¥¦§¨©ª«®¯|@#~½¬-_ç`+*[]{}ḉç¿,.!?:;",&buffer))){
+			for(int j=0; j<numero_palabras;j++){ 
+				int comp = strcmp(token,palabras[j]);
+				if(  comp == 0 ) {
+					//region critica
+					pthread_mutex_lock(&mutex);
+					(num_palabras[j])++;
+					pthread_mutex_unlock(&mutex);
+					//fin region critica
+				}
+			}	
+		}
+		ini=ini+cant-1;
+    }
+  	return (void *)0;
+}
+
 
 int main(int argc, char *argv[]){
 	if(argc>3){
@@ -80,40 +114,51 @@ int main(int argc, char *argv[]){
 
       	int residuo=0,tamanobloque;
 		pthread_t *hilos =malloc(num_hilos*sizeof(pthread_t));
-		
-		//validar tamano de bloque y determinando informacion a enviar en cada hilo
-		//se trata de enviar un cantidad de caracteres aproximidamente igual a cada hilo
-		if(totalcaracteres%num_hilos!=0){
-			tamanobloque=totalcaracteres/num_hilos;
-			residuo=totalcaracteres%num_hilos;
-		}
-		tamanobloque=(totalcaracteres/num_hilos)+residuo;
-		//printf("bloque %d \n", tamanobloque);
 		int numlineas[num_hilos];
-		int cantcaracteres[num_hilos];
 		int numcaracter[num_hilos];
 		int finlinea[num_hilos];
 		int i=0,j=0,total=0;
+		//validar tamano de bloque y determinando informacion a enviar en cada hilo
+		//se trata de enviar un cantidad de caracteres aproximidamente igual a cada hilo
+		if(totallineas<=num_hilos)
+			num_hilos=totallineas;
+		if(totallineas%num_hilos!=0){
+			tamanobloque=totallineas/num_hilos;
+			residuo=totallineas%num_hilos;
+		}
+		tamanobloque=(totallineas/num_hilos);   //numero de lineas para cada hilo
+		printf("bloque de lineas %d \n", tamanobloque);
 		while(i<num_hilos ){
 			numlineas[i]=j;
 			numcaracter[i]=total;
 			int num=0;
-			while(num<tamanobloque && j<=totallineas){
+			while(j<tamanobloque+numlineas[i] && j<=totallineas){
 				num+=tam_lineas[j];
 				j++;
 			}
-			cantcaracteres[i]=num;
 			finlinea[i]=j-1;
+			if(i==num_hilos-1){
+				finlinea[i]=j-1+residuo;
+			}
 			total=total+num;
 			i++;
 		}
 		
 		//imprimiendo la informacion a enviar a cada hilo
 		for(int i=0;i<num_hilos;i++){
-      		printf("hilo : %d numero linea: %d num caracteres: %d  fin linea: %d , cant caracteres: %d \n",
-      		 i+1,numlineas[i],numcaracter[i],finlinea[i], cantcaracteres[i]);
+      		printf("hilo : %d numero linea: %d num caracteres: %d  fin linea: %d  \n",
+      		 i+1,numlineas[i],numcaracter[i],finlinea[i]);
 		}
 		
+		
+		//creando hilo para imprimir estado cada segundo
+		void* retorno=0;
+		pthread_t hilomjs ;
+		int status = pthread_create(&hilomjs, NULL,Estado, NULL);
+			if(status < 0){
+				fprintf(stderr, "Error al crear el hilo mensaje: \n");
+				exit(-1);	
+			}
 		//creando hilos
 		int  hilo;
 		for(hilo=0;hilo<num_hilos;hilo++) {
@@ -139,11 +184,15 @@ int main(int argc, char *argv[]){
 			}
 		}
 
+		//terminar hilo que imprimi mensaje cada segundo
+		pthread_cancel(hilomjs);
+
 		printf("\n \n RESULTADO FINAL busqueda de palabras:\n");
 		for(int i=0;i<numero_palabras;i++){
       		printf("palabra  %s: frecuencia final: %d \n",palabras[i],num_palabras[i]);
       	}
 		pthread_exit(NULL);
+		
 	}else{
 		printf("necesita enviar mas parametros: ./buscar dir_archivo num_hilos palabra1 palabra2..... palabraN.");
 	}
